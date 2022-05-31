184ea605ae09550ecbccb298bcd8bb8490f408c3
ohyuchan <oyuchan50@gmail.com>
2022-05-31 23:43:48 +0900

    Java Grammer(변수 : 연산자)

---
+++ src/ch_02/Operator/ArithmeticOperators/OperatorEx5.java
@@ -0,0 +1,14 @@
+package ch_02.Operator.ArithmeticOperators;
+
+public class OperatorEx5 {
+    public static void main(String[] args){
+        int  a = 10;
+        int  b = 4;
+
+        System.out.printf("%d + %d = %d",a,b,a+b);
+        System.out.printf("%d - %d = %d",a,b,a-b);
+        System.out.printf("%d * %d = %d",a,b,a*b);
+        System.out.printf("%d / %d = %d",a,b,a/b);
+        System.out.printf("%d + %f = %f",a,(float)b,a/(float)b);
+    }
+}
