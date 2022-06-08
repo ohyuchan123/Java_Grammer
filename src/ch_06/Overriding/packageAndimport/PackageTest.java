80df459a7cada273342d95192496894d2fc4c2c5
ohyuchan <oyuchan50@gmail.com>
2022-06-08 20:37:53 +0900

    Java Grammer(객체지향언어II : package와import)

---
+++ src/ch_06/Overriding/packageAndimport/PackageTest.java
@@ -0,0 +1,9 @@
+package ch_06.Overriding.packageAndimport;
+
+public class PackageTest {
+    public static void main(String[] args) {
+        // 패키지의 선언
+        // package 패키지명;
+        System.out.println("Hello world");
+    }
+}
