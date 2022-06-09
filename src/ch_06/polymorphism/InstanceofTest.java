c72ad94276934a4ec99528b1bc522e71318acbd5
ohyuchan <oyuchan50@gmail.com>
2022-06-09 20:06:21 +0900

    Java Grammer(객체지향언어II : 다형성)

---
+++ src/ch_06/polymorphism/InstanceofTest.java
@@ -0,0 +1,17 @@
+package ch_06.polymorphism;
+
+public class InstanceofTest {
+    public static void main(String[] args) {
+        FireEngine1 fe = new FireEngine1();
+
+        if(fe instanceof FireEngine1){
+            System.out.println("This is a FireEngine instance.");
+        }
+        if(fe instanceof Car1){
+            System.out.println("This is a FireEngine instance.");
+        }
+        System.out.println(fe.getClass().getName()); //클래스의 이름을 출력
+    }
+}
+class Car1{}
+class FireEngine1 extends Car1{}
