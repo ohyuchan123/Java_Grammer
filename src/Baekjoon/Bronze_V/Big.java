326f21c3a0580658d87ebf4da7737480de19bb04
ohyuchan <oyuchan50@gmail.com>
2022-06-14 09:06:38 +0900

    Backjoon 문제풀이(브론즈 V : 크냐? BufferedReader에 대해)

--- src/Baekjoon/Bronze_V/Big.java
+++ src/Baekjoon/Bronze_V/Big.java
@@ -9,6 +9,8 @@ import java.util.StringTokenizer;
 public class Big {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // 값을 모아뒀다가 한 번에 전송 BufferedReader은 엔터만 경계로 인식하고 받은 데이터가 String으로 고정되기 때문에 데이터를 
+        //따로 가공해야하나 Scanner에 비해서 상대적으로 빠르다
 
         while(true){
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
