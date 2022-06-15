8231d2243e3fa082687eca22047ec53876d53d66
ohyuchan <oyuchan50@gmail.com>
2022-06-15 15:29:12 +0900

    Backjoon 문제풀이(브론즈 V : Julka)

--- src/Baekjoon/Bronze_V/Julka.java
+++ src/Baekjoon/Bronze_V/Julka.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Julka {
     public static void main(String[] args) throws IOException {
