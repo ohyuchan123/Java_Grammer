ad385683c383c4c00744b3341d0b32af83a6a46f
ohyuchan <oyuchan50@gmail.com>
2022-06-16 16:57:36 +0900

    Backjoon 문제풀이(브론즈 V : ZadanieProbne2)

---
+++ src/Baekjoon/Bronze_V/ZadanieProbne2.java
@@ -0,0 +1,12 @@
+package Baekjoon.Bronze_V;
+
+import java.util.Scanner;
+
+public class ZadanieProbne2 {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        System.out.printf("%d %d",(x+1)*2,(x+1)*3);
+    }
+}
