b2e0c608dd26eee72f264ab10a344c1851d07756
ohyuchan <oyuchan50@gmail.com>
2022-06-23 17:27:44 +0900

    Baekjoon 문제풀이(브론즈 V : 팩토리얼)

---
+++ src/Baekjoon/Bronze_V/Factorial.java
@@ -0,0 +1,21 @@
+package Baekjoon.Bronze_V;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+        int sum = factorial(N);
+        System.out.println(sum);
+
+    }
+
+    public static int factorial(int N) {
+        if(N <= 1) return 1;	// 재귀 종료조건
+        return N * factorial(N - 1);
+    }
+}
