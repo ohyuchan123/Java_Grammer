a68f1c61b5454b1d341121b0e55390057f6248bf
ohyuchan <oyuchan50@gmail.com>
2022-06-27 20:45:52 +0900

    Java의 신(Vol.1 : 자료형(다른 정수형 타입)

---
+++ src/Java의신/VOL1/DataType/checkBoolean.java
@@ -0,0 +1,10 @@
+package Java의신.VOL1.DataType;
+
+public class checkBoolean {
+    public static void main(String[] args) {
+        boolean flag = true;
+        System.out.println(flag);
+        flag = false;
+        System.out.println(flag);
+    }
+}
---
+++ src/Java의신/VOL1/DataType/checkOtherTypes.java
@@ -0,0 +1,7 @@
+package Java의신.VOL1.DataType;
+
+public class checkOtherTypes {
+    short shortMax = 32767;
+    int intMax = 214783647;
+    long longMax = 9223372036854775807l;
+}
