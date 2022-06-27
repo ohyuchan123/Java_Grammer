7e3739aa184219bd6f0089cf1f4b0ca6fcbfec0a
ohyuchan <oyuchan50@gmail.com>
2022-06-27 20:44:30 +0900

    Java의 신(Vol.1 : 자료형(다른 정수형 타입)

---
+++ src/Java의신/VOL1/DataType/checkChar.java
@@ -0,0 +1,16 @@
+package Java의신.VOL1.DataType;
+
+public class checkChar {
+    public static void main(String[] args) {
+        checkChar();
+    }
+    static void checkChar(){
+        char charMin = '\u0000';
+        char charMax = '\uffff';
+        System.out.println("charMin = ["+charMin+"]");
+        System.out.println("charMin = ["+charMax+"]");
+        int intValue = 'a';
+        System.out.println("intValue = ["+intValue+"]");
+
+    }
+}
