845854f8e9a3ebbfab1516dcdf78706595fb50ca
ohyuchan <oyuchan50@gmail.com>
2022-07-02 23:30:05 +0900

    Java의 신(Vol.1 : 하나의 데이터를 여러개에)

--- src/Java의신/VOL1/여러데이터를하나에/Array.java
+++ src/Java의신/VOL1/여러데이터를하나에/Array.java
@@ -6,6 +6,8 @@ public class Array {
     public static void main(String[] args) {
         Array array = new Array();
         array.init();
+        array.primitiveTypes();
+        array.referenceTypes();
     }
     public void init(){
         int[] lottoNumber = new int[7];
@@ -16,5 +18,43 @@ public class Array {
         for(int i=0;i<lottoNumber.length;i++){
             System.out.print(lottoNumber[i]+" ");
         }
+        System.out.println("\n");
+    }
+    public void primitiveTypes(){
+
+        byte[]byteArray = new byte[1];
+        short[]shortArray = new short[1];
+        int[]intArray = new int[1];
+        long[]longArray = new long[1];
+        float[]floatArray = new float[1];
+        double[]doubleArray = new double[1];
+        char[]charArray = new char[1];
+        boolean[]booleanArray = new boolean[1];
+
+        System.out.println("byteArray[0] = "+byteArray[0]);
+        System.out.println("shortArray[0] = "+shortArray[0]);
+        System.out.println("intArray[0] = "+intArray[0]);
+        System.out.println("longArray[0] = "+longArray[0]);
+        System.out.println("floatArray[0] = "+floatArray[0]);
+        System.out.println("doubleArray[0] = "+doubleArray[0]);
+        System.out.println("charArray[0] = "+charArray[0]);
+        System.out.println("booleanArray[0] = "+booleanArray[0]+"\n");
+
+        System.out.println("byteArray = "+byteArray);
+        System.out.println("shortArray = "+shortArray);
+        System.out.println("intArray = "+intArray);
+        System.out.println("longArray = "+longArray);
+        System.out.println("floatArray = "+floatArray);
+        System.out.println("doubleArray = "+doubleArray);
+        System.out.println("charArray = "+charArray);
+        System.out.println("booleanArray = "+booleanArray+"\n");
+
+    }
+    public void referenceTypes(){
+        String[] strings = new String[2];
+        Array[] arrays = new Array[2];
+
+        System.out.println("strings[0] = "+strings[0]);
+        System.out.println("arrays[0] = "+arrays[0]);
     }
 }
