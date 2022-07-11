edf3c8393140d507ec80e528b4844e0997938a0e
ohyuchan <oyuchan50@gmail.com>
2022-07-11 11:31:02 +0900

    Java Grammer(연습)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+
     }
 }
